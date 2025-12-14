5df095196f9954a53fd836cc57f41db229fca3a0
Miguel Vilela <miguelmvilela12@gmail.com>
2025-12-14 12:15:14 -0300

    commit inicial

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-00f46ccd1cbfb363e"
+  instance_type = "t2.micro"
+  key_name = "iac-terraform"
+
+  tags = {
+    Name = "Primeira instancia"
+  }
+}
